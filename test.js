ba90ddcc2d2bdf499c43022f7db4634569be3a58
manbaum <manbaum@hotmail.com>
2016-12-26 17:30:58 +0800

    bug fix in slice(), add functor's test: fid(), fas().

--- test.js
+++ test.js
@@ -34,33 +34,63 @@ console.log(JSON.stringify(t5));
 
 const pure   = FrangeT.pure;
 
-const tIdentity     = v => fcmp(pure($I).apply(v))(v);
-const tComposition = u => v => w => fcmp(pure($compose).apply(u).apply(v).apply(w))(u.apply(v.apply(w)));
-const tHomomorphism = f => x => fcmp(pure(f).apply(pure(x)))(pure(f(x)));
-const tInterchange  = u => y => fcmp(u.apply(pure(y)))(pure(f => f(y)).apply(u));
+const fIdentity     = v => fcmp(v.map($I))($I(v));
+const fAssociative  = f => g => v => fcmp(v.map($compose(f)(g)))($compose(v => v.map(f))(v => v.map(g))(v));
 
-const tid = tIdentity;
-const tco = tComposition;
-const tho = tHomomorphism;
-const tin = tInterchange;
+const fid = fIdentity;
+const fas = fAssociative;
+
+const aIdentity     = v => fcmp(pure($I).apply(v))(v);
+const aComposition  = u => v => w => fcmp(pure($compose).apply(u).apply(v).apply(w))(u.apply(v.apply(w)));
+const aHomomorphism = f => x => fcmp(pure(f).apply(pure(x)))(pure(f(x)));
+const aInterchange  = u => y => fcmp(u.apply(pure(y)))(pure(f => f(y)).apply(u));
+
+const aid = aIdentity;
+const aco = aComposition;
+const aho = aHomomorphism;
+const ain = aInterchange;
+
+const mPure        = v => fcmp(v.mappend())() && fcmp()()
+const mAssociative = v => fcmp()()
 
 let f1 = frange(3)(3);
 let f2 = frange(3)($I);
-let f3 = frange(1)(n => x => n + x);
-let f4 = frange(1)(n => x => [n, x]);
-let f5 = frange(1)(n => x => y => [n + x + y, n * x * y]);
+let f3 = frange(3)(n => x => n + x);
+let f4 = frange(3)(n => x => [n, x]);
+let f5 = frange(3)(n => x => y => [n + x + y, n * x * y]);
 
-console.log(`                    tid(f1): ${tid(f1)}`);
-console.log(`                    tid(f2): ${tid(f2)}`);
-console.log(`            tco(f3)(f3)(f1): ${tco(f3)(f3)(f1)}`);
-console.log(`            tco(f3)(f3)(f2): ${tco(f3)(f3)(f2)}`);
-console.log(`            tco(f4)(f3)(f1): ${tco(f4)(f3)(f1)}`);
-console.log(`            tco(f4)(f3)(f2): ${tco(f4)(f3)(f2)}`);
-console.log(`            tco(f4)(f4)(f1): ${tco(f4)(f4)(f1)}`);
-console.log(`            tco(f4)(f4)(f2): ${tco(f4)(f4)(f2)}`);
-console.log(`tho(x => [x + 1, x * 2])(5): ${tho(x => [x + 1, x * 2])(5)}`);
-console.log(`                 tin(f3)(5): ${tin(f3)(5)}`);
-console.log(`                 tin(f4)(5): ${tin(f4)(5)}`);
+let g1 = x => x + 1;
+let g2 = x => x * 8;
+let g3 = x => [x + 1, x + 2];
+
+console.log(`        fid(f1): ${fid(f1)}`);
+console.log(`        fid(f2): ${fid(f2)}`);
+console.log(`fas(g1)(g1)(f1): ${fas(g1)(g1)(f1)}`);
+console.log(`fas(g1)(g2)(f1): ${fas(g1)(g2)(f1)}`);
+console.log(`fas(g2)(g1)(f1): ${fas(g2)(g1)(f1)}`);
+console.log(`fas(g2)(g2)(f1): ${fas(g2)(g2)(f1)}`);
+console.log(`fas(g3)(g1)(f1): ${fas(g3)(g1)(f1)}`);
+console.log(`fas(g3)(g2)(f1): ${fas(g3)(g2)(f1)}`);
+console.log(`fas(g1)(g1)(f2): ${fas(g1)(g1)(f2)}`);
+console.log(`fas(g1)(g2)(f2): ${fas(g1)(g2)(f2)}`);
+console.log(`fas(g2)(g1)(f2): ${fas(g2)(g1)(f2)}`);
+console.log(`fas(g2)(g2)(f2): ${fas(g2)(g2)(f2)}`);
+console.log(`fas(g3)(g1)(f2): ${fas(g3)(g1)(f2)}`);
+console.log(`fas(g3)(g2)(f2): ${fas(g3)(g2)(f2)}`);
+
+console.log(`        aid(f1): ${aid(f1)}`);
+console.log(`        aid(f2): ${aid(f2)}`);
+console.log(`aco(f3)(f3)(f1): ${aco(f3)(f3)(f1)}`);
+console.log(`aco(f3)(f3)(f2): ${aco(f3)(f3)(f2)}`);
+console.log(`aco(f4)(f3)(f1): ${aco(f4)(f3)(f1)}`);
+console.log(`aco(f4)(f3)(f2): ${aco(f4)(f3)(f2)}`);
+console.log(`aco(f4)(f4)(f1): ${aco(f4)(f4)(f1)}`);
+console.log(`aco(f4)(f4)(f2): ${aco(f4)(f4)(f2)}`);
+console.log(`     aho(g1)(5): ${aho(g1)(5)}`);
+console.log(`     aho(g2)(5): ${aho(g2)(5)}`);
+console.log(`     aho(g3)(5): ${aho(g3)(5)}`);
+console.log(`     ain(f3)(5): ${ain(f3)(5)}`);
+console.log(`     ain(f4)(5): ${ain(f4)(5)}`);
 
 console.log(f1.toArray());
 console.log(f2.toArray());
@@ -77,5 +107,5 @@ const at = fr => i => foldr(x => r => k => k == 0 ? x : r(k - 1))($E(`index.out.
 let fr = frange(10)($I).map(x => frange(10)(n => n * x));
 let r4 = at(fr)(4).toArray();
 console.log(r4);
-let r14 = at(fr)(14);
+try { let r14 = at(fr)(14); } catch (e) { console.log(e.message); }
 // console.log(r14);
